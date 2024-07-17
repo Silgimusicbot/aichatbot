da60fc18a9d28714fa95b9249848188f39285f07
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-07-17 01:43:22 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 18052289))
-API_HASH = getenv("API_HASH", "552525f45a3066fee54ca7852235c19c")
-OWNER_ID = int(getenv("OWNER_ID", "1924693109"))
+API_ID = int(getenv("API_ID", ))
+API_HASH = getenv("API_HASH", )
+OWNER_ID = int(getenv("OWNER_ID", ))
 STRING_SESSION = getenv("STRING_SESSION", "")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 SUPPORT_GRP = getenv("SUPPORT_GRP", "aitsupport")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "aitbots")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "codmastervip")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "atondusalamde")
 
 # Random Start Images
 IMG = [
